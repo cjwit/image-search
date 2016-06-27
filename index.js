ae0c9ac7f223cfc7806cb8d3e822ba3db2b1430d
cjwit <chris.witulski@gmail.com>
2016-06-27 14:31:59 -0400

    working with images custom search

--- index.js
+++ index.js
@@ -1,11 +1,34 @@
 var express = require('express');
 var app = express();
+var config = require('./config');
+var request = require('request');
+var googleAPI = config.googleAPI;
+var searchID = config.searchID;
 
 app.get('/search/:query', function(req, res) {
     var query = req.params.query;
-    res.send(JSON.stringify({
-        query: query
-    }))
+    var url = 'https://www.googleapis.com/customsearch/v1?q=' + query + '&cx=' + searchID + '&key=' + googleAPI;
+    request(url, function(error, response, body) {
+        if (!error && response.statusCode == 200) {
+            var items = JSON.parse(body).items;
+            var results = [];
+            if (!items) {
+                res.send("No results")
+            } else {
+                items.forEach(function(item) {
+                    var result = {
+                        url: item.link,
+                        snippet: item.snippet,
+                        context: item.displayLink
+                    };
+                    results.push(result);
+                })
+                res.send(
+                    results
+                );
+            }
+        };
+    });
 });
 
 var port = process.env.PORT || 8000;
